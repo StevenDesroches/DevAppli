a97eabaf9e1209546af197a90ce70edb381be602
William Durocher <durocherwilliam@gmail.com>
2018-09-19 16:04:21 -0400

    Ajout modele User, ajout zend-db et zend-modulemanager

---
+++ module/Application/src/Model/User.php
@@ -0,0 +1,16 @@
+<?php
+namespace Application\Model;
+
+class Album
+{
+    public $id;
+    public $email;
+    public $password;
+
+    public function exchangeArray(array $data)
+    {
+        $this->id     = !empty($data['id']) ? $data['id'] : null;
+        $this->email = !empty($data['email']) ? $data['email'] : null;
+        $this->password  = !empty($data['password']) ? $data['password'] : null;
+    }
+}
--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -7,12 +7,36 @@
 
 namespace Application;
 
-class Module
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+
+class Module implements ConfigProviderInterface
 {
     const VERSION = '3.0.3-dev';
 
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\UserTable::class => function($container) {
+                    $tableGateway = $container->get(Model\UserTableGateway::class);
+                    return new Model\UserTable($tableGateway);
+                },
+                Model\UserTableGateway::class => function ($container) {
+                    $dbAdapter = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Model\User());
+                    return new TableGateway('user', $dbAdapter, null, $resultSetPrototype);
+                },
+            ],
+        ];
     }
 }
