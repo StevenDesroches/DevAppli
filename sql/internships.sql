0b6d036825a1f4d01aaf94b29ff5abb50aa20f7e
StevenDesroches <stevendesroches@hotmail.com>
2018-12-05 16:54:12 -0500

    Fail, j'ai oublié les relations

--- sql/internships.sql
+++ sql/internships.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 05 Décembre 2018 à 21:47
+-- Généré le :  Mer 05 Décembre 2018 à 21:53
 -- Version du serveur :  5.6.37
 -- Version de PHP :  7.1.8
 
@@ -108,6 +108,7 @@ CREATE TABLE IF NOT EXISTS `students` (
 --
 
 INSERT INTO `students` (`admission_number`, `name`, `active`, `user_id`, `email`) VALUES
+(20, 'steven', 1, 5, ''),
 (1463693, 'Nathan Test 123', 1, 3, ''),
 (69696969, 'Tekashi 69', 1, 4, ''),
 (201463693, 'Nathan Cyr', 0, 1, '');
@@ -134,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `user_type` int(8) NOT NULL COMMENT '''0'' for admin, ''1'' for student, ''2'' for employer'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `users`
@@ -144,7 +145,8 @@ INSERT INTO `users` (`id`, `email`, `password`, `user_type`) VALUES
 (1, 'test@test.com', '098f6bcd4621d373cade4e832627b4f6', 0),
 (2, '201463693', '9a8896ebf2e4a99e4b1c0174504c898a', 1),
 (3, '1463693', '16d7a4fca7442dda3ad93c9a726597e4', 1),
-(4, '69696969', 'cffb9c1f93b6337e59444dbafbd62369', 1);
+(4, '69696969', 'cffb9c1f93b6337e59444dbafbd62369', 1),
+(5, '20', '6ed61d4b80bb0f81937b32418e98adca', 1);
 
 -- --------------------------------------------------------
 
@@ -199,6 +201,13 @@ ALTER TABLE `students`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Index pour la table `students_internships`
+--
+ALTER TABLE `students_internships`
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `internship_id` (`internship_id`);
+
+--
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
@@ -235,7 +244,7 @@ ALTER TABLE `internship_offers`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- Contraintes pour les tables exportées
 --
@@ -263,6 +272,13 @@ ALTER TABLE `internship_offers`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `students_internships`
+--
+ALTER TABLE `students_internships`
+  ADD CONSTRAINT `students_internships_ibfk_1` FOREIGN KEY (`internship_id`) REFERENCES `internship_offers` (`id`),
+  ADD CONSTRAINT `students_internships_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`admission_number`);
 
 --
 -- Contraintes pour la table `users`
