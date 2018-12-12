4dacd99647f068440895c33ee6235a00eec1b7d4
William Durocher <durocherwilliam@gmail.com>
2018-12-12 12:26:53 -0500

    modif bd pour files

--- sql/internships.sql
+++ sql/internships.sql
@@ -1,32 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Mer 05 Décembre 2018 à 21:53
--- Version du serveur :  5.6.37
--- Version de PHP :  7.1.8
+-- Host: localhost:8889
+-- Generation Time: Dec 12, 2018 at 06:26 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.1.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données :  `internships`
+-- Database: `internships`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `coordinators`
+-- Table structure for table `coordinators`
 --
 
-CREATE TABLE IF NOT EXISTS `coordinators` (
+CREATE TABLE `coordinators` (
   `id` int(8) NOT NULL,
   `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -43,10 +37,10 @@ CREATE TABLE IF NOT EXISTS `coordinators` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `employers`
+-- Table structure for table `employers`
 --
 
-CREATE TABLE IF NOT EXISTS `employers` (
+CREATE TABLE `employers` (
   `id` int(8) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `contact_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -57,10 +51,10 @@ CREATE TABLE IF NOT EXISTS `employers` (
   `active` tinyint(1) NOT NULL,
   `id_user` int(8) NOT NULL,
   `uuid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `employers`
+-- Dumping data for table `employers`
 --
 
 INSERT INTO `employers` (`id`, `name`, `contact_email`, `adress`, `city`, `province`, `postal_code`, `active`, `id_user`, `uuid`) VALUES
@@ -69,20 +63,31 @@ INSERT INTO `employers` (`id`, `name`, `contact_email`, `adress`, `city`, `provi
 -- --------------------------------------------------------
 
 --
--- Structure de la table `internship_offers`
+-- Table structure for table `files`
 --
 
-CREATE TABLE IF NOT EXISTS `internship_offers` (
+CREATE TABLE `files` (
+  `file` text NOT NULL,
+  `student_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `internship_offers`
+--
+
+CREATE TABLE `internship_offers` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `date_posted` datetime NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `active` tinyint(1) NOT NULL,
   `id_employer` int(8) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `internship_offers`
+-- Dumping data for table `internship_offers`
 --
 
 INSERT INTO `internship_offers` (`id`, `name`, `date_posted`, `description`, `active`, `id_employer`) VALUES
@@ -92,10 +97,10 @@ INSERT INTO `internship_offers` (`id`, `name`, `date_posted`, `description`, `ac
 -- --------------------------------------------------------
 
 --
--- Structure de la table `students`
+-- Table structure for table `students`
 --
 
-CREATE TABLE IF NOT EXISTS `students` (
+CREATE TABLE `students` (
   `admission_number` int(8) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `active` tinyint(4) NOT NULL,
@@ -104,11 +109,12 @@ CREATE TABLE IF NOT EXISTS `students` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `students`
+-- Dumping data for table `students`
 --
 
 INSERT INTO `students` (`admission_number`, `name`, `active`, `user_id`, `email`) VALUES
 (20, 'steven', 1, 5, ''),
+(12345, 'will', 1, 6, 'will@test.com'),
 (1463693, 'Nathan Test 123', 1, 3, ''),
 (69696969, 'Tekashi 69', 1, 4, ''),
 (201463693, 'Nathan Cyr', 0, 1, '');
@@ -116,10 +122,10 @@ INSERT INTO `students` (`admission_number`, `name`, `active`, `user_id`, `email`
 -- --------------------------------------------------------
 
 --
--- Structure de la table `students_internships`
+-- Table structure for table `students_internships`
 --
 
-CREATE TABLE IF NOT EXISTS `students_internships` (
+CREATE TABLE `students_internships` (
   `student_id` int(8) NOT NULL,
   `internship_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -127,18 +133,18 @@ CREATE TABLE IF NOT EXISTS `students_internships` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(8) NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `user_type` int(8) NOT NULL COMMENT '''0'' for admin, ''1'' for student, ''2'' for employer'
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `user_type`) VALUES
@@ -146,21 +152,22 @@ INSERT INTO `users` (`id`, `email`, `password`, `user_type`) VALUES
 (2, '201463693', '9a8896ebf2e4a99e4b1c0174504c898a', 1),
 (3, '1463693', '16d7a4fca7442dda3ad93c9a726597e4', 1),
 (4, '69696969', 'cffb9c1f93b6337e59444dbafbd62369', 1),
-(5, '20', '6ed61d4b80bb0f81937b32418e98adca', 1);
+(5, '20', '6ed61d4b80bb0f81937b32418e98adca', 1),
+(6, '12345', '098f6bcd4621d373cade4e832627b4f6', 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user_types`
+-- Table structure for table `user_types`
 --
 
-CREATE TABLE IF NOT EXISTS `user_types` (
+CREATE TABLE `user_types` (
   `id` int(11) NOT NULL,
   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `user_types`
+-- Dumping data for table `user_types`
 --
 
 INSERT INTO `user_types` (`id`, `type`) VALUES
@@ -169,46 +176,52 @@ INSERT INTO `user_types` (`id`, `type`) VALUES
 (2, 'employer');
 
 --
--- Index pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `coordinators`
+-- Indexes for table `coordinators`
 --
 ALTER TABLE `coordinators`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Index pour la table `employers`
+-- Indexes for table `employers`
 --
 ALTER TABLE `employers`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_user` (`id_user`);
 
 --
--- Index pour la table `internship_offers`
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD KEY `fk_students_files` (`student_id`);
+
+--
+-- Indexes for table `internship_offers`
 --
 ALTER TABLE `internship_offers`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_employer` (`id_employer`);
 
 --
--- Index pour la table `students`
+-- Indexes for table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`admission_number`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Index pour la table `students_internships`
+-- Indexes for table `students_internships`
 --
 ALTER TABLE `students_internships`
   ADD KEY `student_id` (`student_id`),
   ADD KEY `internship_id` (`internship_id`);
 
 --
--- Index pour la table `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -216,76 +229,82 @@ ALTER TABLE `users`
   ADD KEY `user_type` (`user_type`);
 
 --
--- Index pour la table `user_types`
+-- Indexes for table `user_types`
 --
 ALTER TABLE `user_types`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `coordinators`
+-- AUTO_INCREMENT for table `coordinators`
 --
 ALTER TABLE `coordinators`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT pour la table `employers`
+-- AUTO_INCREMENT for table `employers`
 --
 ALTER TABLE `employers`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT pour la table `internship_offers`
+-- AUTO_INCREMENT for table `internship_offers`
 --
 ALTER TABLE `internship_offers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
--- Contraintes pour les tables exportées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `coordinators`
+-- Constraints for table `coordinators`
 --
 ALTER TABLE `coordinators`
   ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Contraintes pour la table `employers`
+-- Constraints for table `employers`
 --
 ALTER TABLE `employers`
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
 
 --
--- Contraintes pour la table `internship_offers`
+-- Constraints for table `files`
+--
+ALTER TABLE `files`
+  ADD CONSTRAINT `fk_students_files` FOREIGN KEY (`student_id`) REFERENCES `students` (`admission_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `internship_offers`
 --
 ALTER TABLE `internship_offers`
   ADD CONSTRAINT `fk_id_employer` FOREIGN KEY (`id_employer`) REFERENCES `employers` (`id`);
 
 --
--- Contraintes pour la table `students`
+-- Constraints for table `students`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Contraintes pour la table `students_internships`
+-- Constraints for table `students_internships`
 --
 ALTER TABLE `students_internships`
   ADD CONSTRAINT `students_internships_ibfk_1` FOREIGN KEY (`internship_id`) REFERENCES `internship_offers` (`id`),
   ADD CONSTRAINT `students_internships_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`admission_number`);
 
 --
--- Contraintes pour la table `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `fk_user_types` FOREIGN KEY (`user_type`) REFERENCES `user_types` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
